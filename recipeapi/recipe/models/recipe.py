6842c7ae01d37b58325566ae0019c059dbf1f87d
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-11-10 10:14:14 -0600

    fixtures for users, recipes, ingredients

--- recipeapi/recipe/models/recipe.py
+++ recipeapi/recipe/models/recipe.py
@@ -6,6 +6,8 @@ from .ingredient import Ingredient
 class Recipe(models.Model):
 
     name = models.CharField(max_length=150)
-    description = models.TextField()
+    description = models.CharField(max_length=150)
+    instructions = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
     ingredients = models.ManyToManyField(Ingredient)
+    favorites = models.ManyToManyField(User)
--- recipeapi/seed_data.sh
+++ recipeapi/seed_data.sh
@@ -1,3 +1,6 @@
 rm ./db.sqlite3
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py loaddata users
+python3 manage.py loaddata ingredients
+python3 manage.py loaddata recipe
